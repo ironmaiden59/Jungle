896d803cee91e52aac52d8e5d7ebd5ca22046f2c
ralph <ralphdcouto1987@gmail.com>
2023-10-26 00:52:47 -0400

    feature spec home page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,19 @@
+describe('home page', () => {
+
+  it('can visit the homepage', () => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+});
